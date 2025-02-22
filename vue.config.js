9faabca630f27c5307ec7000cdf5f27997a800ed
KentaNakano <tanukilubangno@gmail.com>
2025-02-22 19:49:32 +0900

    fix: デプロイ用に設定ファイルを修正

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   // github pagesへデプロイ用の設定
-  outputDir: 'docs',
   assetsDir: './',
   publicPath: './'
 });
